4c64d745500a7f3698229afb9a231df93cc0c009
sankethacr <sanketha.cr@reltio.com>
2020-08-15 23:46:41 +0530

    Initial commit

---
+++ spring-boot-jpa-mockito/src/main/java/com/springboot/app/springbootjpamockito/SpringBootJpaMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.springbootjpamockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaMockitoApplication.class, args);
+	}
+
+}
