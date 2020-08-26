a4725808d0128d36ec0e29deb2d9f69c517b318e
sankethacr <sanketha.cr@reltio.com>
2020-08-26 18:38:03 +0530

    Updated config

--- spring-boot-jpa-mockito/src/main/java/com/springboot/app/model/Person.java
+++ spring-boot-jpa-mockito/src/main/java/com/springboot/app/model/Person.java
@@ -1,10 +1,20 @@
 package com.springboot.app.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Person_Table")
 public class Person {
 
 	@Id
@@ -13,51 +23,9 @@ public class Person {
 	private String name;
 	private int age;
 
-	public Person() {
-		super();
-		System.out.println("Default constructor");
-	}
-
-	public Person(long personId, String name, int age) {
-		super();
-		this.personId = personId;
-		this.name = name;
-		this.age = age;
-	}
-
 	public Person(String name, int age) {
 		super();
-		this.name = name;
 		this.age = age;
-	}
-
-	public long getPersonId() {
-		return personId;
-	}
-
-	public void setPersonId(long personId) {
-		this.personId = personId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
 		this.name = name;
 	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [personId=" + personId + ", name=" + name + ", age=" + age + "]";
-	}
-
 }
